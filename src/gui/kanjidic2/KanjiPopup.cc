Gnurou/tagainijisho

commit d67b7b92b5b0ffbc8d950944f59aebc87ee5fddc

Fixed possibly uninitialized variable

src/gui/kanjidic2/KanjiPopup.cc
@@ -48,6 +48,7 @@ GraphicsComponentItem::GraphicsComponentItem(int width, const KanjiComponent *co
 		original = EntriesCache::get(KANJIDIC2ENTRY_GLOBALID, TextTools::singleCharToUnicode(component->original()));
 		kOriginal = static_cast<const Kanjidic2Entry *>(original.data());
 	}
+	else return;
 	QFont font;
 	QFontMetrics metrics(font);
 	_meaningsHeight = metrics.height();

commit 6c125e3b2711c7feca446c7478ce0249b17c6475

Fixed bug introduced by d67b7b92b5b0ffbc8d950944f59aebc87ee5fddc

src/gui/kanjidic2/KanjiPopup.cc
@@ -43,19 +43,18 @@ GraphicsComponentItem::GraphicsComponentItem(int width, const KanjiComponent *co
 	_kanji = EntriesCache::get(KANJIDIC2ENTRY_GLOBALID, TextTools::singleCharToUnicode(component->element()));
 
 	EntryPointer<const Entry> original;
-	const Kanjidic2Entry *kOriginal;
+	const Kanjidic2Entry *kOriginal(0);
 	if (!component->original().isEmpty()) {
 		original = EntriesCache::get(KANJIDIC2ENTRY_GLOBALID, TextTools::singleCharToUnicode(component->original()));
 		kOriginal = static_cast<const Kanjidic2Entry *>(original.data());
 	}
-	else return;
 	QFont font;
 	QFontMetrics metrics(font);
 	_meaningsHeight = metrics.height();
 	_readingsHeight = metrics.height();
 	_kanjiSize = _meaningsHeight + _readingsHeight;
 	QString meanings;
-	if (original.data()) meanings = kOriginal->meaningsString();
+	if (kOriginal) meanings = kOriginal->meaningsString();
 	else meanings = kanji()->meaningsString();
 	if (!meanings.isEmpty()) meanings[0] = meanings[0].toUpper();
 	_meanings = metrics.elidedText(meanings, Qt::ElideRight, width - _kanjiSize);