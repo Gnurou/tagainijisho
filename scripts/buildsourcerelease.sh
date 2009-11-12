a086fb8fbd5f6b679ee718ce2f872bab5f37a573
Alexandre Courbot <gnurou@gmail.com>
2009-11-12 20:44:52 +0900

    Fixed source tarball build script

--- scripts/buildsourcerelease.sh
+++ scripts/buildsourcerelease.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-VERSION=`grep "^ *VERSION" tagainijisho.pro |sed -n 's/.*=\(.*\)/\1/p'`
-bzr export tagainijisho-${VERSION}
+VERSION=`grep "VERSION" buildconfig.pri |sed -n 's/.*=\(.*\)/\1/p'`
+git archive --format=tar --prefix=tagainijisho-$VERSION/ HEAD |tar xv
 cd tagainijisho-${VERSION}
+lrelease src/gui/gui.pro
 ./builddb.py -d
 cd ..
 tar cvzf tagainijisho-${VERSION}.tar.gz tagainijisho-${VERSION}
