6ea125095b877dd75358de2610a3a517d73c9b8d
Alexandre Courbot <gnurou@gmail.com>
2010-01-14 18:20:04 +0900

    C++ XML parser taking shape

--- src/core/kanjidic2/BuildKanjiDB.cc
+++ src/core/kanjidic2/BuildKanjiDB.cc
@@ -22,33 +22,114 @@
 
 #include <QtDebug>
 
-static bool process_literal(QXmlStreamReader &reader)
-{
-	while(!reader.atEnd()) {
-		QXmlStreamReader::TokenType ttype = reader.readNext();
-		if (ttype == QXmlStreamReader::Characters) {
-			int kanjiCode = TextTools::singleCharToUnicode(reader.text().toString());
-			qDebug() << TextTools::unicodeToSingleChar(kanjiCode);
-		}
-		else if (ttype == QXmlStreamReader::EndElement && reader.name() == "literal") return false;
-	}
+typedef struct {
+	int id;
+	int grade;
+	int strCount;
+	int freq;
+	int jlpt;
+	QList<QString> skip;
+	QList<QString> readings;
+	QList<QString> meanings;
+	QList<QString> nanori;
+} Kanji;
+
+#define __IGNORE if (ttype == QXmlStreamReader::Comment || ttype == QXmlStreamReader::DTD) continue;
+
+#define PROCESS_BEGIN(tag, reader) \
+	while (!reader.atEnd()) {     \
+		QXmlStreamReader::TokenType ttype = reader.readNext(); \
+		__IGNORE \
+		if (ttype == QXmlStreamReader::EndElement && reader.name() == #tag) return false;
+
+#define PROCESS_END return true; \
+	} \
 	return true;
+
+#define DOCUMENT_BEGIN(reader) \
+	if (reader.readNext() != QXmlStreamReader::StartDocument) return true; \
+	while (!reader.atEnd()) {     \
+		QXmlStreamReader::TokenType ttype = reader.readNext(); \
+		__IGNORE
+		
+#define DOCUMENT_END \
+		} \
+		return reader.tokenType() != QXmlStreamReader::EndDocument;
+
+#define CHARACTERS if (ttype == QXmlStreamReader::Characters) {
+#define TEXT reader.text()
+#define IGNORE_CHARACTERS CHARACTERS DONE
+
+#define TAG(n) if (ttype == QXmlStreamReader::StartElement && reader.name() == #n) {
+#define DONE continue; }
+#define PASS(n) do reader.readNext(); while (!reader.atEnd() && !(reader.tokenType() == QXmlStreamReader::EndElement && reader.name() == #n));
+#define IGNORE_OTHER_TAGS if (ttype == QXmlStreamReader::StartElement) { \
+	QStringRef tName(reader.name()); \
+	do reader.readNext(); while (!reader.atEnd() && !(reader.tokenType() == QXmlStreamReader::EndElement && reader.name() == tName)); \
+	continue; \
+	}
+
+#define PROCESS(part, ...) { if (process##_##part(__VA_ARGS__)) return true; }
+
+static bool process_literal(QXmlStreamReader &reader, Kanji &kanji)
+{
+	PROCESS_BEGIN(literal, reader)
+		CHARACTERS
+			int kanjiCode = TextTools::singleCharToUnicode(TEXT.toString());
+			kanji.id = kanjiCode;
+		DONE
+	PROCESS_END
 }
 
-static bool process_character(QXmlStreamReader &reader)
+static bool process_character(QXmlStreamReader &reader, Kanji &kanji)
 {
-	QXmlStreamAttributes attrs = reader.attributes();
-	while(!reader.atEnd()) {
-		QXmlStreamReader::TokenType ttype = reader.readNext();
-		if (ttype == QXmlStreamReader::EndElement && reader.name() == "character") {
-			// Character is completely read, we can write it into the database and return
-			return false;
-		}
-		else if (ttype == QXmlStreamReader::StartElement) {
-			if (reader.name() == "literal") if (process_literal(reader)) return true;
-		}
-	}
-	return true;
+	PROCESS_BEGIN(character, reader)
+		TAG(literal)
+			PROCESS(literal, reader, kanji)
+		DONE
+		IGNORE_OTHER_TAGS
+		IGNORE_CHARACTERS
+	PROCESS_END
+}
+
+static bool process_header(QXmlStreamReader &reader)
+{
+	PROCESS_BEGIN(header, reader)
+		TAG(file_version)
+			PASS(file_version)
+		DONE
+		TAG(database_version)
+			PASS(database_version)
+		DONE
+		TAG(date_of_creation)
+			PASS(date_of_creation)
+		DONE
+		IGNORE_CHARACTERS
+	PROCESS_END
+}
+
+static bool process_kanjidic2(QXmlStreamReader &reader)
+{
+	PROCESS_BEGIN(kanjidic2, reader)
+		TAG(character)
+			Kanji kanji;
+			PROCESS(character, reader, kanji)
+		DONE
+		TAG(header)
+			PROCESS(header, reader);
+		DONE
+		IGNORE_CHARACTERS
+	PROCESS_END
+}
+
+static bool process_main(QXmlStreamReader &reader)
+{
+	DOCUMENT_BEGIN(reader)
+		TAG(kanjidic2)
+			PROCESS(kanjidic2, reader)
+		DONE
+		IGNORE_CHARACTERS
+	DOCUMENT_END
 }
 
 int main(int argc, char *argv[])
@@ -57,15 +138,8 @@ int main(int argc, char *argv[])
 	QFile file("3rdparty/kanjidic2.xml");
 	QXmlStreamReader reader(&file);
 	file.open(QFile::ReadOnly | QFile::Text);
-
-	while(!reader.atEnd()) {
-		QXmlStreamReader::TokenType ttype = reader.readNext();
-		if (ttype == QXmlStreamReader::StartElement) {
-			if (reader.name() == "character") if (process_character(reader)) {
-				qDebug() << "Error while processing";
-			}
-		}
+	if (process_main(reader)) {
+		return 1;
 	}
-	
 	return 0;
 }
