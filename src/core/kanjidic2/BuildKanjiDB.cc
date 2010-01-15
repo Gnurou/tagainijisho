466b4384025699c834b38a131ce5220f639fbdfe
Alexandre Courbot <gnurou@gmail.com>
2010-01-15 19:12:01 +0900

    More work on kanji database builder

--- src/core/kanjidic2/BuildKanjiDB.cc
+++ src/core/kanjidic2/BuildKanjiDB.cc
@@ -25,6 +25,11 @@
 #include <QtDebug>
 
 typedef struct {
+private:
+	static QSqlQuery insertEntryQuery;
+	static QSqlQuery insertReadingQuery;
+	
+public:
 	int id;
 	int grade;
 	int strCount;
@@ -34,7 +39,36 @@ typedef struct {
 	QList<QString> readings;
 	QList<QString> meanings;
 	QList<QString> nanori;
+	
+	static void initializeQueries(QSqlDatabase &database);
+	
+	bool insertIntoDatabase();
 } Kanji;
+QSqlQuery Kanji::insertEntryQuery;
+QSqlQuery Kanji::insertReadingQuery;
+
+void Kanji::initializeQueries(QSqlDatabase& database)
+{
+	insertEntryQuery = QSqlQuery(database);
+	insertEntryQuery.prepare("insert into entries values(?, ?, ?, ?, ?)");
+	insertReadingQuery = QSqlQuery(database);
+	insertReadingQuery.prepare("insert into reading values(?, ?, ?)");
+}
+
+/**
+ * Returns true if the entry has successfully been inserted, false otherwise.
+ */
+bool Kanji::insertIntoDatabase()
+{
+	insertEntryQuery.addBindValue(id);
+	insertEntryQuery.addBindValue(grade);
+	insertEntryQuery.addBindValue(strCount);
+	insertEntryQuery.addBindValue(freq);
+	insertEntryQuery.addBindValue(jlpt);
+	bool res = insertEntryQuery.exec();
+	if (!res) qDebug() << insertEntryQuery.lastError();
+	return res;
+}
 
 #define __IGNORE if (ttype == QXmlStreamReader::Comment || ttype == QXmlStreamReader::DTD) continue;
 
@@ -86,6 +120,7 @@ PROCESS_END
 
 PROCESS_BEGIN(character, Kanji &kanji)
 	TAG(literal)
+	// TODO Allow to embed CHARACTERS in order to avoid defining a new function - use cases instead of new functions
 		PROCESS(literal, reader, kanji)
 	DONE
 	IGNORE_OTHER_TAGS
@@ -109,6 +144,7 @@ PROCESS_BEGIN(kanjidic2)
 	TAG(character)
 		Kanji kanji;
 		PROCESS(character, reader, kanji)
+		if (!kanji.insertIntoDatabase()) return true;
 	DONE
 	TAG(header)
 		PROCESS(header, reader);
@@ -133,6 +169,35 @@ void create_tables()
 	query.prepare("insert into info values(?)");
 	query.addBindValue(KANJIDIC2DB_REVISION);
 	query.exec();
+	query.exec("create table entries(id INTEGER PRIMARY KEY, grade TINYINT, strokeCount TINYINT, frequency SMALLINT, jlpt TINYINT)");
+	query.exec("create table reading(docid INTEGER PRIMARY KEY, entry INTEGER SECONDARY KEY REFERENCES entries, type TEXT)");
+	query.exec("create virtual table readingText using fts3(reading, TOKENIZE katakana)");
+	query.exec("create table meaning(docid INTEGER PRIMARY KEY, entry INTEGER SECONDARY KEY REFERENCES entries, lang TEXT)");
+	query.exec("create virtual table meaningText using fts3(reading)");
+	query.exec("create table nanori(docid INTEGER PRIMARY KEY, entry INTEGER SECONDARY KEY REFERENCES entries)");
+	query.exec("create virtual table nanoriText using fts3(reading, TOKENIZE katakana)");
+
+	query.exec("create table strokeGroups(kanji INTEGER, parentGroup INTEGER SECONDARY KEY REFERENCES strokeGroups, number TINYINT, element INTEGER, original INTEGER)");
+	query.exec("create table strokes(parentGroup INTEGER SECONDARY KEY REFERENCES strokeGroups, strokeType INTEGER, path TEXT)");
+	query.exec("create table skip(entry INTEGER, type TINYINT, c1 TINYINT, c2 TINYINT)");
+}
+
+void create_indexes()
+{
+	QSqlQuery query;
+	query.exec("create index idx_entries_frequency on entries(frequency)");
+	query.exec("create index idx_jlpt on entries(jlpt)");
+	query.exec("create index idx_reading_entry on reading(entry)");
+	query.exec("create index idx_meaning_entry on meaning(entry)");
+	query.exec("create index idx_nanori_entry on nanori(entry)");
+	query.exec("create index idx_strokeGroups_kanji on strokeGroups(kanji)");
+	query.exec("create index idx_strokeGroups_parentGroup on strokeGroups(parentGroup)");
+	query.exec("create index idx_strokeGroups_element on strokeGroups(element)");
+	query.exec("create index idx_strokeGroups_original on strokeGroups(original)");
+	query.exec("create index idx_strokes_parentGroup on strokes(parentGroup)");
+	query.exec("create index idx_strokes_strokeType on strokes(strokeType)");
+	query.exec("create index idx_skip on skip(entry)");
+	query.exec("create index idx_skip_type on skip(type, c1, c2)");
 }
 
 int main(int argc, char *argv[])
@@ -147,23 +212,34 @@ int main(int argc, char *argv[])
 		qFatal("Cannot open database: %s", database.lastError().text().toLatin1().data());
 		return 1;
 	}
-	create_tables();
-
-	// Attach custom functions
+	// Attach custom functions and tokenizers
 	QVariant handler = database.driver()->handle();
 	if (handler.isValid() && !qstrcmp(handler.typeName(), "sqlite3*")) {
 		sqlite3 *sqliteHandler = *static_cast<sqlite3 **>(handler.data());
-		// TODO Move into dedicated open function? Since it cannot be used
+		// TODO Move into dedicated open function? Since it cannot use
 		// the sqlite3_auto_extension
 		register_all_tokenizers(sqliteHandler);
 	}
 	
-	
+	// Now open the file to parse
 	QFile file("3rdparty/kanjidic2.xml");
 	QXmlStreamReader reader(&file);
 	file.open(QFile::ReadOnly | QFile::Text);
+	
+	// Let's go
+	database.transaction();
+	create_tables();
+
+	// Prepare the queries
+	Kanji::initializeQueries(database);
 	if (process_main(reader)) {
 		return 1;
 	}
+	
+	create_indexes();
+	
+	// Commit everything
+	database.commit();
+	qDebug() << "Commited\n";
 	return 0;
 }
