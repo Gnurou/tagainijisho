becf567a0ddcf87d94f4174be413746f1b281a3a
Alexandre Courbot <gnurou@gmail.com>
2010-02-19 13:08:20 +0900

    Added option to enable/disable entries cache debugging

--- src/config.h.cmake
+++ src/config.h.cmake
@@ -2,4 +2,6 @@
 #define CONFIG_H__
 #cmakedefine VERSION ${VERSION}
 #cmakedefine DATA_DIR ${DATA_DIR}
+
+#cmakedefine DEBUG_ENTRIES_CACHE ${DEBUG_ENTRIES_CACHE}
 #endif
--- src/core/EntriesCache.cc
+++ src/core/EntriesCache.cc
@@ -15,6 +15,7 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "config.h"
 #include "core/EntriesCache.h"
 #include "core/EntrySearcherManager.h"
 
