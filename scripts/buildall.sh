56003b05be6d58b982b321f9f0f10d7724e6ea8e
Alexandre Courbot <gnurou@gmail.com>
2010-03-12 18:27:30 +0900

    Fixed release script

--- scripts/buildall.sh
+++ scripts/buildall.sh
@@ -2,6 +2,7 @@
 # Build all that is possible to build under Linux (source tarball + databases + win32 binaries)
 VERSION=`grep "set(VERSION " CMakeLists.txt |sed -n 's/set(VERSION \(.*\))$/\1/p'`
 
+rm -Rf releasefiles
 mkdir releasefiles
 sh scripts/buildsourcerelease.sh
 tar xfz tagainijisho-$VERSION.tar.gz
@@ -11,4 +12,5 @@ sh scripts/buildwin32releases.sh
 mv *.db tagainijisho-*.exe ../releasefiles
 cd ..
 mv tagainijisho-$VERSION.tar.gz releasefiles
+rm -Rf tagainijisho-$VERSION
 
