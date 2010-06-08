a15d1ea34a3339a735983b5536cad89c356cd23e
Alexandre Courbot <gnurou@gmail.com>
2010-06-08 15:35:40 +0900

    Skeleton of HTML template for kanji entries

--- src/gui/kanjidic2/Kanjidic2EntryFormatter.cc
+++ src/gui/kanjidic2/Kanjidic2EntryFormatter.cc
@@ -94,7 +94,7 @@ Kanjidic2EntryFormatter &Kanjidic2EntryFormatter::instance()
 	return _instance;
 }
 
-Kanjidic2EntryFormatter::Kanjidic2EntryFormatter(QObject* parent) : EntryFormatter("detailed_kanji.css", "detailed_kanji.html", parent)
+Kanjidic2EntryFormatter::Kanjidic2EntryFormatter(QObject* parent) : EntryFormatter("detailed_kanjidic2.css", "detailed_kanjidic2.html", parent)
 {
 }
 
