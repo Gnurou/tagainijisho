7afc21cb042c34987b5bd70c244a1dd004e3b4f0
Alexandre Courbot <gnurou@gmail.com>
2010-06-20 12:01:33 +0900

    Small fixes

--- src/sqlite/Error.h
+++ src/sqlite/Error.h
@@ -13,24 +13,7 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *//*
-    <one line to give the program's name and a brief idea of what it does.>
-    Copyright (C) <year>  <name of author>
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-*/
+ */
 
 #ifndef __SQLITE_ERROR_H
 #define __SQLITE_ERROR_H
@@ -46,3 +29,4 @@ public:
 }
 
 #endif
+
