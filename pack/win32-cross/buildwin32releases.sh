6da158aae99f70b8ae640411948bea9451d8618a
Alexandre Courbot <gnurou@gmail.com>
2010-08-05 16:17:01 +0900

    Fix win32 release build scripts

--- pack/win32-cross/buildwin32releases.sh
+++ pack/win32-cross/buildwin32releases.sh
@@ -1,29 +1,32 @@
-#!/bin/sh
-VERSION=`grep "set(VERSION " CMakeLists.txt |sed "s/set(VERSION \(.*\))/\1/"`
-QTPATH=`grep "^set(QT_ROOT" scripts/Toolchain-win32.cmake |sed "s/set(QT_ROOT \(.*\))/\1/"`
+#!/bin/bash
+curpath=`pwd`
+BASEDIR=`readlink -f \`dirname $0\``
+SRCDIR=`readlink -f $BASEDIR/../..`
+BUILDDIR=$BASEDIR/build
+VERSION=`grep "set(VERSION " $SRCDIR/CMakeLists.txt |sed "s/set(VERSION \(.*\))/\1/"`
+TOOLCHAIN=$BASEDIR/Toolchain-win32.cmake
+QTPATH=`grep "^set(QT_ROOT" $TOOLCHAIN |sed "s/set(QT_ROOT \(.*\))/\1/"`
+NSIFILE=$BASEDIR/tagainijisho.nsi
 MINGWDLLPATH=${MINGWDLLPATH-/usr/i486-mingw32/lib}
-DBDIR=${DBDIR-.}
-NSIFILE=${NSIFILE-tagainijisho.nsi}
-BUILDDIR=build-win32
 
-mkdir win32-translations
+mkdir -p $BUILDDIR
 for lang in fr de es ru nl;
 do
-	touch win32-translations/qt_$lang.qm
-	lrelease $QTPATH/translations/qt_$lang.ts -qm win32-translations/qt_$lang.qm
+	touch $BUILDDIR/qt_$lang.qm
+	lrelease $QTPATH/translations/qt_$lang.ts -qm $BUILDDIR/qt_$lang.qm
 done
 
-mkdir $BUILDDIR
 cd $BUILDDIR
-cmake -DCMAKE_TOOLCHAIN_FILE=../scripts/Toolchain-win32.cmake -DCMAKE_BUILD_TYPE=Release ..
-make tagainijisho
-cd ..
+cmake -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN -DCMAKE_BUILD_TYPE=Release $SRCDIR
+make tagainijisho -j5
+rm -f CMakeCache.txt
+cmake -DCMAKE_BUILD_TYPE=Release -DDICT_LANG="en;fr;de;es;ru" $SRCDIR
+make i18n databases -j5
 
+cd $BASEDIR
 for lang in en fr de es ru;
 do
-	makensis -DLANG=$lang -DVERSION=$VERSION -DBUILDDIR=$BUILDDIR -DDBDIR=$DBDIR -DQTPATH=$QTPATH -DMINGWDLLPATH=$MINGWDLLPATH $NSIFILE
-	mv install.exe tagainijisho-${VERSION}-${lang}.exe
+	makensis -DLANG=$lang -DVERSION=$VERSION -DBUILDDIR=$BUILDDIR -DSRCDIR=$SRCDIR -DQTPATH=$QTPATH -DMINGWDLLPATH=$MINGWDLLPATH $NSIFILE
+	mv install.exe tagainijisho-$VERSION-$lang.exe
 done
-
-rm -Rf win32-translations
-
+cd $curpath
