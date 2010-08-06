fb20f1c21d8dec1dce9e46cdf8bae6d9e73e0242
Alexandre Courbot <gnurou@gmail.com>
2010-08-06 11:58:14 +0900

    Fix release script

--- pack/release/uploadrelease.py
+++ pack/release/uploadrelease.py
@@ -90,13 +90,13 @@ for release in project.releases:
 	if release.version == lpRelease:
 		gpgPass = getpass.getpass("Found the release. I will now create signature files - please enter your GPG private key passphrase: ")
 		# Upload the source tarball
-		uploadFile('src/pack/source/tagainijisho-' + releaseVersion + '.tar.gz', 'source', '', gpgPass)
+		uploadFile('pack/source/tagainijisho-' + releaseVersion + '.tar.gz', 'source', '', gpgPass)
 		# Upload the mac binaries
 		for lang in LANGUAGES:
-			uploadFile('src/pack/MacOS/Tagaini Jisho-' + releaseVersion + '-' + LANGUAGES_SUFFIXES[lang] + '.dmg', 'mac', lang, gpgPass)
+			uploadFile('pack/MacOS/Tagaini Jisho-' + releaseVersion + '-' + LANGUAGES_SUFFIXES[lang] + '.dmg', 'mac', lang, gpgPass)
 		# Upload the win32 binaries
 		for lang in LANGUAGES:
-			uploadFile('src/pack/win32-cross/tagainijisho-' + releaseVersion + '-' + LANGUAGES_SUFFIXES[lang] + '.exe', 'win32', lang, gpgPass)
+			uploadFile('pack/win32-cross/tagainijisho-' + releaseVersion + '-' + LANGUAGES_SUFFIXES[lang] + '.exe', 'win32', lang, gpgPass)
 		release.lp_save()
 		sys.exit(0)
 print "Release not found - please create it on Launchpad first."
