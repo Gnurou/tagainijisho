cd254c67f1bfc551424dc126657c86102b5bbeb2
Alexandre Courbot <gnurou@gmail.com>
2010-08-06 15:11:08 +0900

    Source release script now updates translations

--- pack/source/buildsourcerelease.sh
+++ pack/source/buildsourcerelease.sh
@@ -5,6 +5,9 @@ BASEDIR=`readlink -f \`dirname $0\``
 SRCDIR=`readlink -f $BASEDIR/../..`
 VERSION=`grep "set(VERSION " $SRCDIR/CMakeLists.txt |sed -n 's/set(VERSION \(.*\))$/\1/p'`
 cd $SRCDIR
+lupdate . -ts i18n/*.ts
+git add i18n
+git commit i18n -m "Updated translation files for version $VERSION"
 git archive --format=tar --prefix=gitexport/ HEAD |tar xv -C $BASEDIR
 cd $BASEDIR
 mkdir -p build
