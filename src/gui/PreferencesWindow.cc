ee4c1000062b425dc03fc8ab7ed2669c4ea4255f
Alexandre Courbot <gnurou@gmail.com>
2010-08-18 16:25:32 +0900

    Languages in the preferences are not translated and instead written in their native form

--- src/gui/PreferencesWindow.cc
+++ src/gui/PreferencesWindow.cc
@@ -87,12 +87,13 @@ void PreferencesWindow::applySettings() {
 }
 
 const QList<QPair<QString, QString> > GeneralPreferences::langs = (QList<QPair<QString, QString> >() <<
-	QPair<QString, QString>(QT_TRANSLATE_NOOP("GeneralPreferences", "English"), "en") <<
-	QPair<QString, QString>(QT_TRANSLATE_NOOP("GeneralPreferences", "Dutch"), "nl") <<
-	QPair<QString, QString>(QT_TRANSLATE_NOOP("GeneralPreferences", "French"), "fr") <<
-	QPair<QString, QString>(QT_TRANSLATE_NOOP("GeneralPreferences", "German"), "de") <<
-	QPair<QString, QString>(QT_TRANSLATE_NOOP("GeneralPreferences", "Russian"), "ru") <<
-	QPair<QString, QString>(QT_TRANSLATE_NOOP("GeneralPreferences", "Spanish"), "es"));
+	QPair<QString, QString>(QString::fromUtf8("English"), "en") <<
+	QPair<QString, QString>(QString::fromUtf8("Français (French)"), "fr") <<
+	QPair<QString, QString>(QString::fromUtf8("Deutsch (German)"), "de") <<
+	QPair<QString, QString>(QString::fromUtf8("Nederlands (Dutch)"), "nl") <<
+	QPair<QString, QString>(QString::fromUtf8("Русский (Russian)"), "ru") <<
+	QPair<QString, QString>(QString::fromUtf8("Español (Spanish)"), "es")
+	);
 
 GeneralPreferences::GeneralPreferences(QWidget *parent) : PreferencesWindowCategory(tr("General"), parent)
 {
