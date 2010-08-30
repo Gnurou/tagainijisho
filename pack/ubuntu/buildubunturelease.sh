db813d360786cad8c84c6827c2c17785da23a971
Alexandre Courbot <gnurou@gmail.com>
2010-08-30 12:20:24 +0900

    Fix debian release script

--- pack/ubuntu/buildubunturelease.sh
+++ pack/ubuntu/buildubunturelease.sh
@@ -4,8 +4,8 @@ BASEDIR=`readlink -f \`dirname $0\``
 SRCDIR=`readlink -f $BASEDIR/../..`
 VERSION=`grep "set(VERSION " $SRCDIR/CMakeLists.txt |sed -n 's/set(VERSION \(.*\))$/\1/p'`
 cd $SRCDIR
-cp $SRCDIR/tagainijisho-$VERSION.tar.gz $SRCDIR/tagainijisho_$VERSION.orig.tar.gz
+cp $SRCDIR/../tagainijisho-$VERSION.tar.gz $SRCDIR/../tagainijisho_$VERSION.orig.tar.gz
 cp -R $SRCDIR/pack/ubuntu/debian $SRCDIR
-dch -e .
+dch
 debuild -S -sa
-rm $SRCDIR/tagainijisho_$VERSION.orig.tar.gz
+rm $SRCDIR/../tagainijisho_$VERSION.orig.tar.gz
