2c7d317be0cddef467b9f8daf64f81c4e21a842a
Alexandre Courbot <gnurou@gmail.com>
2010-12-09 14:18:42 +0900

    Add dummy proxy model to lists view in order to start filtering according to the user-set root list

---
+++ src/core/EntryListModelFilter.h
@@ -0,0 +1,35 @@
+/*
+ *  Copyright (C) 2010  Alexandre Courbot
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __CORE_ENTRYLISTFILTERMODEL_H
+#define __CORE_ENTRYLISTFILTERMODEL_H
+
+#include "core/EntryListModel.h"
+#include <QSortFilterProxyModel>
+
+class EntryListModelFilter : public QSortFilterProxyModel
+{
+	Q_OBJECT
+private:
+
+public:
+	EntryListModelFilter(QObject *parent = 0);
+	virtual QModelIndex mapFromSource(const QModelIndex &sourceIndex) const;
+	virtual QModelIndex mapToSource(const QModelIndex &proxyIndex) const;
+};
+
+#endif
