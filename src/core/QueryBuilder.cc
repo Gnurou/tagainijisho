06665f088c9280ae2be0d68ee2fa10fe45640d0b
Alexandre Courbot <gnurou@gmail.com>
2011-10-30 16:21:35 +0900

    Strenghten query builder with explicit parentheses - harmonize case in SQL statements

--- src/core/QueryBuilder.cc
+++ src/core/QueryBuilder.cc
@@ -62,16 +62,17 @@ QString QueryBuilder::Join::toString(const Column &with) const
 	QString res;
 	switch (type()) {
 	case Join::Cross:
-		res += "join ";
+		res += "JOIN ";
 		break;
 	case Join::Left:
-		res += "left join ";
+		res += "LEFT JOIN ";
 		break;
 	}
-	res += table1() + " on " + column1().toString() + " = ";
+	res += table1() + " ON (" + column1().toString() + " = ";
 	if (!hasRightPart()) res += with.toString();
 	else res += column2().toString();
-	if (additionalCondition() != "") res += " and " + additionalCondition();
+	res += ")";
+	if (additionalCondition() != "") res += " AND (" + additionalCondition() + ")";
 	return res;
 }
 
@@ -105,7 +106,7 @@ QString QueryBuilder::GroupBy::toString() const
 QString QueryBuilder::Limit::toString() const
 {
 	if (!active()) return "";
-	else return QString("limit %1,%2").arg(start()).arg(nbResults());
+	else return QString("LIMIT %1,%2").arg(start()).arg(nbResults());
 }
 
 int QueryBuilder::Statement::addColumn(const Column &column, int pos)
@@ -161,7 +162,7 @@ QString QueryBuilder::Statement::sqlStatementRightPart() const
 	}
 
 	if (!_joins.isEmpty()) {
-		res += " from ";
+		res += " FROM ";
 
 		leftJoin = &jList[0];
 		res += leftJoin->column1().table();
@@ -173,15 +174,15 @@ QString QueryBuilder::Statement::sqlStatementRightPart() const
 	}
 
 	if (!_wheres.isEmpty() || (leftJoin && leftJoin->hasAdditionalCondition())) {
-		res += " where ";
+		res += " WHERE ";
 
 		QStringList whereStrs;
-		if (leftJoin->hasAdditionalCondition()) whereStrs << leftJoin->additionalCondition();
+		if (leftJoin->hasAdditionalCondition()) whereStrs << "(" + leftJoin->additionalCondition() + ")";
 
 		foreach (const Where &where, wheres()) {
-			whereStrs << where.toString();
+			whereStrs << "(" + where.toString() + ")";
 		}
-		res += whereStrs.join(" and ");
+		res += whereStrs.join(" AND ");
 	}
 
 	return res;
@@ -197,9 +198,9 @@ QString QueryBuilder::Statement::sqlStatementGroupPart() const
 
 QString QueryBuilder::Statement::buildSqlStatement() const
 {
-	QString res = "select ";
+	QString res = "SELECT ";
 
-	if (distinct()) res += "distinct ";
+	if (distinct()) res += "DISTINCT ";
 
 	for (int i = 0; i < _columns.size(); i++) {
 		if (i > 0) res += ", ";
