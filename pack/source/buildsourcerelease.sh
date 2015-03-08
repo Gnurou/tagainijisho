0e3b70fbddc45da24f573956b89ccbb40ae96a9e
Alexandre Courbot <gnurou@gmail.com>
2015-03-08 11:54:51 +0900

    pack/source: reuse 3rdparty files

--- pack/source/buildsourcerelease.sh
+++ pack/source/buildsourcerelease.sh
@@ -4,17 +4,13 @@ curpath=`pwd`
 BASEDIR=`readlink -f \`dirname $0\``
 SRCDIR=`readlink -f $BASEDIR/../..`
 VERSION=`grep "set(VERSION " $SRCDIR/CMakeLists.txt |sed -n 's/set(VERSION \(.*\))$/\1/p'`
-#cd $SRCDIR/i18n
-#./update.sh
-#git add .	
-#git commit -m "Updated translation files for version $VERSION"
 cd $SRCDIR
 git archive --format=tar --prefix=gitexport/ HEAD |tar xv -C $BASEDIR
 cd $BASEDIR
+cp -R $SRCDIR/3rdparty gitexport/
 mkdir -p build
 cd build
-cmake ../gitexport
-cmake -DEMBED_SQLITE=1 .
+cmake -DEMBED_SQLITE=1 ../gitexport
 make docs
 make package_source
 mv tagainijisho-${VERSION}.tar.gz $curpath
