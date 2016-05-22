fbe3823cf097d56d38b04e86e006d59f1d4af6b1
Alexandre Courbot <gnurou@gmail.com>
2016-05-22 18:50:47 +0900

    pack: win32: update build script to use new CPack rules
    Update the build script to use the CPack rules. This results
    in a shorter, safer script.

--- pack/win32-cross/buildwin32releases.sh
+++ pack/win32-cross/buildwin32releases.sh
@@ -1,30 +1,24 @@
 #!/bin/bash
+set -e
+
 curpath=`pwd`
 BASEDIR=`readlink -f \`dirname $0\``
 SRCDIR=`readlink -f $BASEDIR/../..`
 BUILDDIR=$BASEDIR/build-win32
-VERSION=`grep "set(VERSION " $SRCDIR/CMakeLists.txt |sed "s/set(VERSION \(.*\))/\1/"`
-QTPATH=/usr/i686-w64-mingw32
-QTTRANS=/home/gnurou/.wine/drive_c/Qt/latest
-NSIFILE=$BASEDIR/tagainijisho.nsi
 
-mkdir -p $BUILDDIR/i18n
-for lang in `ls $SRCDIR/i18n/*.ts |cut -d'_' -f2 |cut -d'.' -f1`;
-do
-	touch $BUILDDIR/i18n/qt_$lang.qm
-	lrelease $QTTRANS/translations/qt_$lang.ts -qm $BUILDDIR/i18n/qt_$lang.qm
-done
-
+# Build databases and docs using the native toolchain
+mkdir -p $BUILDDIR
 cd $BUILDDIR
-cmake -DCMAKE_BUILD_TYPE=Release -DEMBED_SQLITE=1 $SRCDIR
+cmake -DCMAKE_BUILD_TYPE=Release $SRCDIR
 make i18n databases docs -j5
-rm -Rf CMakeCache.txt CMakeFiles
-i686-w64-mingw32-cmake -DCMAKE_BUILD_TYPE=Release -DEMBED_SQLITE=1 $SRCDIR
-make tagainijisho -j5
-i686-w64-mingw32-strip $BUILDDIR/src/gui/tagainijisho.exe
 
-cd $BASEDIR
-makensis -DVERSION=$VERSION -DBUILDDIR=$BUILDDIR -DSRCDIR=$SRCDIR -DQTPATH=$QTPATH $NSIFILE
-mv install.exe $curpath/tagainijisho-$VERSION.exe
+# Build everything else using the cross toolchain
+rm -Rf CMakeCache.txt CMakeFiles
+x86_64-w64-mingw32-cmake -DCMAKE_BUILD_TYPE=Release $SRCDIR
+make -j5
+
+cpack -G NSIS64
+
+mv tagainijisho-$VERSION.exe $curpath
 rm -Rf $BUILDDIR
 cd $curpath
