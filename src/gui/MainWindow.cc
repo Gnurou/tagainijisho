4da2f09df7f02b7ab26752c1623a14d3b3dc9ed9
Alexandre Courbot <gnurou@gmail.com>
2026-01-11 15:19:12 +0900

    MainWindow: call Qt6-compatible QPixmap constructor

--- src/gui/MainWindow.cc
+++ src/gui/MainWindow.cc
@@ -332,7 +332,7 @@ void MainWindow::about() {
     aboutDialog.setWindowIcon(windowIcon());
     aboutDialogUI.title->setText(aboutDialogUI.title->text() + " " + VERSION);
     aboutDialogUI.logo->setPixmap(
-        aboutDialogUI.logo->pixmap()->scaledToWidth(75, Qt::SmoothTransformation));
+        aboutDialogUI.logo->pixmap(Qt::ReturnByValue).scaledToWidth(75, Qt::SmoothTransformation));
     aboutDialogUI.credits->setHtml(message + credits);
     aboutDialogUI.credits->viewport()->setAutoFillBackground(false);
     ScrollBarSmoothScroller scroller;
